7f82091115b285d43a70a577ade485f7828fa9fe
Pianoplayer1 <alexander@capitain.me>
2021-05-02 19:52:51 +0200

    Change bot.py to use a class

--- bot.py
+++ bot.py
@@ -3,25 +3,34 @@ from discord.ext import commands
 from functions import db
 from os import listdir, getenv
 
-async def get_prefixes(client, message):
-    prefixes = [f'<@!{client.user.id}> ', f'<@{client.user.id}> ']
-    if message.guild is None:
-        prefixes.append('-')
-    else:
-        prefixes.append(db.query("SELECT * FROM servers WHERE id = %s", message.guild.id)[0][1])
-    return prefixes
+class Pianobot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.members = True
+        super().__init__(command_prefix = self.get_prefixes, help_command = None, intents = intents)
 
-client = commands.Bot(command_prefix = get_prefixes, help_command = None, intents = discord.Intents(guilds=True,members=True,messages=True,reactions=True))
+        self.load_extension_folder('commands/test')
+        self.load_extension_folder('events')
 
-for filename in listdir('./commands'):
-    if filename.endswith('.py'):
-        client.load_extension(f'commands.{filename[:-3]}')
-for filename in listdir('./events'):
-    if filename.endswith('.py'):
-        client.load_extension(f'events.{filename[:-3]}')
+    async def get_prefixes(self, client, message):
+        prefixes = [f'<@!{self.user.id}> ', f'<@{self.user.id}> ']
+        if message.guild is None:
+            prefixes.append('-')
+        else:
+            prefixes.extend([prefix for row in db.query("SELECT prefix FROM servers WHERE id = %s", message.guild.id) for prefix in row])
+        return prefixes
+
+    def load_extension_folder(self, path):
+        for extension in [filename[:-3] for filename in listdir(f'./{path}') if filename.endswith('.py')]:
+            try:
+                self.load_extension(f'{path.replace("/", ".")}.{extension}')
+            except Exception as e:
+                print(f'Could not load ./{path}/{extension}')
+
+client = Pianobot()
 
 db.connect()
 client.run(getenv('TOKEN'))
 
 db.disconnect()
-print('Exited bot')
+print('Shutting down.')
