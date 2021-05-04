358cad932a9a190d50880bb1d8874195dafdaca4
Pianoplayer1 <alexander@capitain.me>
2021-05-04 19:10:32 +0200

    test

--- commands/members.py
+++ commands/members.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 from functions.db import query
-import aiohttp
 
 class Members(commands.Cog):
 
@@ -13,6 +12,18 @@ class Members(commands.Cog):
                         help = 'This command outputs members of the Eden Discord server.',
                         usage = ''  )
     async def members(self, ctx):
+        output = {'Missing link' : [], 'Wrong amount of roles' : []}
+
+        links = dict(query('SELECT uuid, discord FROM members'))
+        with self.session.get('https://api.wynncraft.com/public_api.php?action=guildStats&command=Eden') as response:
+            ingame_members = response.json()['members']
+        for member in ingame_members:
+            try:
+                if links[member['uuid']] == 0:
+                    raise KeyError
+            except KeyError:
+                output['Missing link'].append(member['name'])
+
         eden = self.client.get_guild(682671629213368351)
         roles = {'guild_member' : eden.get_role(682675039631310915),
                  'citizen'      : eden.get_role(703591550424317963),
@@ -22,11 +33,10 @@ class Members(commands.Cog):
                  'dux'          : eden.get_role(682675116865224773),
                  'consul'       : eden.get_role(727551690248683571),
                  'emperor'      : eden.get_role(682677588933869577)}
-        guild_members = [member for member in eden.members if roles['guild_member'] in member.roles]
-        wrong_amount = [member for member in guild_members if sum(role in member.roles for role in roles.values()) > 2]
+        discord_members = [member for member in eden.members if roles['guild_member'] in member.roles]
+        output['Wrong amount of roles'] = [member.nickname for member in discord_members if sum(role in member.roles for role in roles.values()) > 2]
 
-        await ctx.send(', '.join(w.name for w in wrong_amount))
-
+        await ctx.send(output)
 
 def setup(client):
     client.add_cog(Members(client))
