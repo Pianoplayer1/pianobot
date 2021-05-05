ce3885f2bd057bf86ca0abe56a56b2f6cdaf4421
Pianoplayer1 <alexander@capitain.me>
2021-05-05 18:12:49 +0200

    Update members.py

--- commands/members.py
+++ commands/members.py
@@ -26,7 +26,15 @@ class Members(commands.Cog):
                 else:
                     print(f'Couldn\'t find uuid of {name}')
                 return
-        output = {'Missing link':[],'Wrong amount of roles':[],'No guild member role':[],'Not in the guild':[],'Wrong role':[],'Wrong nickname':[]}
+
+
+        output = {  'Dormant, but still in the guild - can be kicked if needed'
+                    'Missing link between MC and Discord account' : [],
+                    'Wrong amount of Discord roles' : [],
+                    'No guild member role in Discord' : [],
+                    'Not in the ingame guild' : [],
+                    'Highest Discord role not matching ingame role' : [],
+                    'Discord nickname not matching ingame name or rank symbol' : []  }
 
         links = dict(query('SELECT discord, uuid FROM members'))
         async with self.client.session.get('https://api.wynncraft.com/public_api.php?action=guildStats&command=Eden') as response:
@@ -34,41 +42,54 @@ class Members(commands.Cog):
         ingame_members = {member['uuid'] : {'name': member['name'], 'rank': member['rank']} for member in json_response['members']}
 
         eden = self.client.get_guild(682671629213368351)
-        roles = {'guild_member' : eden.get_role(682675039631310915),
-                 'recruit'      : eden.get_role(703591550424317963),
-                 'recruiter'    : eden.get_role(703591538508300299),
-                 'captain'      : eden.get_role(682675300307697675),
-                 'strategist'   : eden.get_role(727549491699253379),
-                 'chief'        : eden.get_role(682675116865224773),
-                 'consul'       : eden.get_role(727551690248683571),
-                 'owner'        : eden.get_role(682677588933869577)}
+        roles = {   'guild_member' : eden.get_role(682675039631310915),
+                    'recruit'      : eden.get_role(703591550424317963),
+                    'recruiter'    : eden.get_role(703591538508300299),
+                    'captain'      : eden.get_role(682675300307697675),
+                    'strategist'   : eden.get_role(727549491699253379),
+                    'chief'        : eden.get_role(682675116865224773),
+                    'consul'       : eden.get_role(727551690248683571),
+                    'owner'        : eden.get_role(682677588933869577) }
+        dormant_role = eden.get_role(755160797956669610)
+        symbols = {roles['recruit'] : '', roles['recruiter'] : '◉ ', roles['captain'] : '♞ ', roles['strategist'] : '♝ ', roles['chief'] : '♜ ', roles['consul'] : '♕ ', roles['owner'] : '♔ '}
         high_roles = [roles['owner'], roles['consul'], roles['chief'], roles['strategist']]
 
         for member in eden.members:
+            discord_name = member.nick or member.name
             if roles['guild_member'] in member.roles:
-                if sum(role in member.roles for role in roles.values()) != 2:
-                    output['Wrong amount of roles'].append(member.nick or member.name)
+                dormant = dormant_role in member.roles
                 try:
                     uuid = links[member.id]
-                    try:
-                        ingame_member = ingame_members[uuid]
-                        if roles[ingame_member['rank'].lower()] in member.roles:
-                            if ingame_member['name'] not in (member.nick or member.name) and all(role not in member.roles for role in [high_roles]):
-                                output['Wrong nickname'].append(member.nick or member.name)
-                        elif roles['consul'] not in member.roles:
-                            output['Wrong role'].append(member.nick or member.name)
-                    except KeyError:
-                        output['Not in the guild'].append(member.nick or member.name)
                 except KeyError:
-                    output['Missing link'].append(member.nick or member.name)
+                    output['Missing link between MC and Discord account'].append(discord_name)
+                    continue
+                try:
+                    ingame_name = ingame_members[uuid]['name']
+                    ingame_rank = ingame_members[uuid]['rank'].lower()
+                except KeyError:
+                    if not dormant:
+                        output['Not in the ingame guild'].append(discord_name)
+                    continue
+                temp_roles = member.roles
+                temp_roles.reverse()
+                highest_role = next(role for role in temp_roles if role in roles)
+
+                if dormant:
+                    output['Dormant, but still in the guild - can be kicked if needed'].append(discord_name)
+
+                if sum(role in member.roles for role in roles.values()) != 2:
+                    output['Wrong amount of Discord roles'].append(discord_name)
+
+                if highest_role not in [roles[ingame_rank], roles['consul']]:
+                    output['Highest Discord role not matching ingame role'].append(discord_name)
+
+                if discord_name != symbols[highest_role] + ingame_members[uuid]['name'] and highest_role not in high_roles:
+                    output['Discord nickname not matching ingame name or rank symbol'].append(discord_name)
+
             elif any(role in member.roles for role in roles.values()) and 'administrator' not in permissions(member, eden.channels[0]):
-                output['No guild member role'].append(member.nick or member.name)
-        message = ''
-        for category in output.items():
-            if len(category[1]) > 0:
-                message += f'\n\n**{category[0]}:**\n'
-                message += '\n'.join(category[1])
-        await ctx.send(message)
+                output['No guild member role in Discord'].append(discord_name)
+
+        await ctx.send('\n'.join(f'\n**{category[0]}:**\n' + '\n'.join(category[1]) for category in output.items() if len(category[1]) > 0))
 
 def setup(client):
     client.add_cog(Members(client))
