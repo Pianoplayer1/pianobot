9fd0befbe253abb27b550ee2c9ce0ebb6317dd84
Pianoplayer1 <alexander@capitain.me>
2021-05-16 20:26:44 +0200

    Fix members.py

--- commands/members.py
+++ commands/members.py
@@ -63,7 +63,7 @@ class Members(commands.Cog):
         for member in eden.members:
             discord_name = member.nick or member.name
             message_name = member.mention
-            if any(role in high_roles for role in roles['guild_member']):
+            if any(role in high_roles for role in member.roles):
                 message_name = discord_name
             if roles['guild_member'] in member.roles:
                 dormant = dormant_role in member.roles
