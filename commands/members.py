a42faee5d718a1b81f25237d59020beec9975af9
Pianoplayer1 <74676416+Pianoplayer1@users.noreply.github.com>
2021-05-24 09:58:32 +0200

    Update members.py

--- commands/members.py
+++ commands/members.py
@@ -14,17 +14,19 @@ class Members(commands.Cog):
                         usage = ''  )
     async def members(self, ctx, *, args = None):
         if args is not None and check_permissions(ctx.author, ctx.channel, ['manage_roles']):
-            command, name, id = args.split()
-            if command.lower() == 'link':
-                async with self.client.session.get(f'https://api.mojang.com/users/profiles/minecraft/{name}') as response:
-                    json_response = await response.json()
-                uuid = json_response['id']
-                name = json_response['name']
-                if uuid:
-                    uuid = uuid[0:8]+'-'+uuid[8:12]+'-'+uuid[12:16]+'-'+uuid[16:20]+'-'+uuid[20:32]
-                    query('INSERT INTO members VALUES(%s, %s, %s, 0, 0, 0, 0, 0)', (name, uuid, id))
-                else:
-                    print(f'Couldn\'t find uuid of {name}')
+            try:
+                command, name, id = args.split()
+                if command.lower() == 'link':
+                    async with self.client.session.get(f'https://api.mojang.com/users/profiles/minecraft/{name}') as response:
+                        json_response = await response.json()
+                    uuid = json_response['id']
+                    name = json_response['name']
+                    if uuid:
+                        uuid = uuid[0:8]+'-'+uuid[8:12]+'-'+uuid[12:16]+'-'+uuid[16:20]+'-'+uuid[20:32]
+                        query('INSERT INTO members VALUES(%s, %s, %s, 0, 0, 0, 0, 0)', (name, uuid, id))
+                    else:
+                        await ctx.send(f'Couldn\'t find uuid of {name}')
+            finally:
                 return
 
 
@@ -47,8 +49,8 @@ class Members(commands.Cog):
                     'consul'       : eden.get_role(727551690248683571),
                     'owner'        : eden.get_role(682677588933869577) }
         dormant_role = eden.get_role(755160797956669610)
-        symbols = {roles['recruit'] : '', roles['recruiter'] : '◉ ', roles['captain'] : '♞ ', roles['strategist'] : '♝ ', roles['chief'] : '♜ ', roles['consul'] : '♕ ', roles['owner'] : '♔ '}
-        high_roles = [roles['owner'], roles['consul'], roles['chief'], roles['strategist']]
+        rank_symbols = {roles['recruit'] : '', roles['recruiter'] : '◉ ', roles['captain'] : '♞ ', roles['strategist'] : '♝ ', roles['chief'] : '♜ ', roles['consul'] : '♕ ', roles['owner'] : '♔ '}
+        senate_roles = [roles['owner'], roles['consul'], roles['chief'], roles['strategist']]
 
         links = dict(query('SELECT discord, uuid FROM members'))
         async with self.client.session.get('https://api.wynncraft.com/public_api.php?action=guildStats&command=Eden') as response:
@@ -56,17 +58,15 @@ class Members(commands.Cog):
         ingame_members = {}
         for member in json_response['members']:
             if member['uuid'] not in links.values():
-                output['Unknown Discord account'].append(member['name'].replace("_", "\_"))
+                output['Unknown Discord account'].append(member['name'].replace('_', '\_'))
             else:
                 ingame_members[member['uuid']] = {'name': member['name'], 'rank': member['rank']}
 
         for member in eden.members:
             discord_name = member.nick or member.name
-            message_name = member.mention
-            if any(role in high_roles for role in member.roles):
-                message_name = discord_name
+            message_name = member.mention if any(role in high_roles for role in member.roles) else discord_name
             if roles['guild_member'] in member.roles:
-                dormant = dormant_role in member.roles
+                is_dormant = dormant_role in member.roles
                 try:
                     uuid = links[member.id]
                     if uuid == '0':
@@ -78,27 +78,28 @@ class Members(commands.Cog):
                     ingame_name = ingame_members[uuid]['name']
                     ingame_rank = ingame_members[uuid]['rank'].lower()
                 except KeyError:
-                    if not dormant:
+                    if not is_dormant:
                         output['Not in the ingame guild'].append(message_name)
                     continue
                 temp_roles = member.roles
                 temp_roles.reverse()
                 highest_role = next(role for role in temp_roles if role in roles.values())
 
-                if dormant:
+                if is_dormant:
                     output['Dormant, but still in the guild - can be kicked if needed'].append(message_name)
 
                 if sum(role in member.roles for role in roles.values()) != 2:
                     output['Wrong amount of Discord roles'].append(message_name)
 
-                if highest_role in high_roles:
+                if highest_role in senate_roles:
                     continue
 
                 if highest_role != roles[ingame_rank]:
-                    output['Highest Discord role not matching ingame role'].append(message_name + ' (' + ingame_rank.capitalize() + ' ingame)')
+                    output['Highest Discord role not matching ingame role'].append(f'{message_name} ({ingame_rank} ingame)')
 
-                if discord_name != symbols[highest_role] + ingame_members[uuid]['name']:
-                    output['Discord nickname not matching ingame name or rank symbol'].append(message_name + ' --> ' + symbols[highest_role] + ingame_members[uuid]['name'].replace('_', '\_'))
+                correct_name = rank_symbols[highest_role] + ingame_members[uuid]['name']
+                if discord_name != correct_name:
+                    output['Discord nickname not matching ingame name or rank symbol'].append(message_name + ' --> ' + correct_name.replace('_', '\_'))
 
             elif any(role in member.roles for role in roles.values()) and 'administrator' not in permissions(member, eden.channels[0]):
                 output['No guild member role in Discord'].append(message_name)
