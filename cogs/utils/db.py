21d897f064977f3e68f19665ced9f6cc9866b356
Pianoplayer1 <alexander@capitain.me>
2021-12-07 19:02:32 +0100

    fix db.py

--- cogs/utils/db.py
+++ cogs/utils/db.py
@@ -1,16 +1,17 @@
 from os import getenv
 import psycopg2
+from psycopg2 import connection
 
-def connect() -> psycopg2.connection:
+def connect() -> connection:
     con = psycopg2.connect(host = getenv('PG_HOST'), user = getenv('PG_USER'), password = getenv('PG_PASS'), database = getenv('PG_DB'))
     print('Connected to database')
     return con
 
-def disconnect(con : psycopg2.connection):
+def disconnect(con : connection):
     con.close()
     print('Disconnected from database')
 
-def query(con : psycopg2.connection, sql : str, vals : tuple) -> tuple:
+def query(con : connection, sql : str, vals : tuple) -> tuple:
     sql = sql.replace('`', '"')
     cursor = con.cursor()
     cursor.execute(sql, vals)
