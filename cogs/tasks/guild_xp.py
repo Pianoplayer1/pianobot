9d564fc50143b8a374f55c8ae3bbca7d49485a27
Pianoplayer1 <alexander@capitain.me>
2021-12-08 17:12:34 +0100

    change guild_xp.py

--- cogs/tasks/guild_xp.py
+++ cogs/tasks/guild_xp.py
@@ -6,14 +6,12 @@ async def run(bot : Pianobot) -> None:
     async with bot.session.get('https://api.wynncraft.com/public_api.php?action=guildStats&command=Eden') as response:
         response = await response.json()
         current_xp = {member['name']: member['contributed'] for member in response['members']}
-        columns = bot.query('SELECT "column_name" FROM "information_schema"."columns" WHERE "table_name" = \'guildXP\';')
-        xp_values = bot.query('SELECT * FROM "guildXP" ORDER BY "time" DESC LIMIT 1;')
-        last_xp = {columns[i][0]: xp_values[0][i] for i in range(1, len(columns))}
+        columns = [column[0] for column in bot.query('SELECT "column_name" FROM "information_schema"."columns" WHERE "table_name" = \'guildXP\';')[1:]]
         
         to_add = set(current_xp.keys())
-        to_add.difference_update(last_xp.keys())
+        to_add.difference_update(columns)
         add_string = ', '.join([f'ADD COLUMN "{name}" VARCHAR NOT NULL' for name in to_add])
-        to_remove = set(last_xp.keys())
+        to_remove = set(columns)
         to_remove.difference_update(current_xp.keys())
         remove_string = ', '.join([f'DROP COLUMN IF EXISTS "{name}"' for name in to_remove])
         if add_string and remove_string:
