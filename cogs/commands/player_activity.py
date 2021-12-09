6c2518ac33cce40a21fddc0d66cc78f165367d2f
Pianoplayer1 <74676416+Pianoplayer1@users.noreply.github.com>
2021-12-09 08:05:39 +0100

    change player_activity.py

--- cogs/commands/player_activity.py
+++ cogs/commands/player_activity.py
@@ -1,7 +1,7 @@
 from ..bot import Pianobot
 from discord.ext import commands
 from discord import File
-from matplotlib import pyplot, dates
+import urllib
 
 class PlayerActivity(commands.Cog):
     def __init__(self, bot : Pianobot):
@@ -13,19 +13,7 @@ class PlayerActivity(commands.Cog):
                         help = 'This command returns a bar graph with the number of minutes a given player has been online in the last days.',
                         usage = '<player> [days]')
     async def graph(self, ctx : commands.Context, player : str, interval : int = 14):
-        res = self.bot.query(f'SELECT `date`, `count` FROM `playerActivity` WHERE `name` = CONCAT(`date`, \'_{player}\') AND `date` > (CURRENT_TIMESTAMP - \'{interval} day\'::interval);')
-        data = {time: amount for time, amount in res}
-
-        plot, axes = pyplot.subplots()
-        bar = axes.bar(data.keys(), data.values())
-        axes.xaxis.set_major_locator(dates.DayLocator(interval=1))  
-        axes.xaxis.set_major_formatter(dates.DateFormatter('%a'))
-        axes.xaxis.set_label_position('top')
-        axes.bar_label(bar, label_type='center', color='white')
-        pyplot.xlabel(f'Player Activity of {player}')
-        pyplot.ylabel('Minutes')
-        plot.savefig('pact.png')
-
+        urllib.URLOpener().retrieve(f'https://wynnstats.dieterblancke.xyz/api/charts/onlinetime/{player}/{interval}', 'pact.png')
         await ctx.send(file = File('pact.png'))
 
 def setup(bot : Pianobot):
