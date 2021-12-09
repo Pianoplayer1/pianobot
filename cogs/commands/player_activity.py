17e653522f10658b2032d3fbed90e5d5e2fb92cb
Pianoplayer1 <74676416+Pianoplayer1@users.noreply.github.com>
2021-12-09 08:12:51 +0100

    fix player_activity.py

--- cogs/commands/player_activity.py
+++ cogs/commands/player_activity.py
@@ -1,7 +1,6 @@
 from ..bot import Pianobot
 from discord.ext import commands
 from discord import File
-import urllib
 
 class PlayerActivity(commands.Cog):
     def __init__(self, bot : Pianobot):
@@ -13,8 +12,7 @@ class PlayerActivity(commands.Cog):
                         help = 'This command returns a bar graph with the number of minutes a given player has been online in the last days.',
                         usage = '<player> [days]')
     async def graph(self, ctx : commands.Context, player : str, interval : int = 14):
-        urllib.URLopener().retrieve(f'https://wynnstats.dieterblancke.xyz/api/charts/onlinetime/{player}/{interval}', 'pact.png')
-        await ctx.send(file = File('pact.png'))
+        await ctx.send(f'https://wynnstats.dieterblancke.xyz/api/charts/onlinetime/{player}/{interval}')
 
 def setup(bot : Pianobot):
     bot.add_cog(PlayerActivity(bot))
