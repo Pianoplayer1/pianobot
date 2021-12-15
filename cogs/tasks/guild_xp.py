46c4ee6caa75ccf78bae46c454f341450a3435eb
Pianoplayer1 <alexander@capitain.me>
2021-12-15 16:39:44 +0100

    fix guild_xp.py

--- cogs/tasks/guild_xp.py
+++ cogs/tasks/guild_xp.py
@@ -35,19 +35,20 @@ async def run(bot : Pianobot) -> None:
         logging.getLogger('tasks').debug('Duplicate guild xp time')
     
     data = bot.query('SELECT * FROM "guildXP" ORDER BY time DESC LIMIT 2;')
-    members = list(current_xp.keys())
+    members = [column[0] for column in bot.query('SELECT column_name FROM information_schema.columns WHERE table_name = \'guildXP\';')[1:]]
     xp_diff = [[members[i], data[0][i + 1] - data[1][i + 1]] for i in range(len(members)) if data[0][i + 1] - data[1][i + 1] > 0]
     if len(xp_diff) == 0: return
 
     msg = '--------------------------------------------------------------------------------'
     for pos, (name, xp) in enumerate(sorted(xp_diff, key = lambda item: item[1], reverse = True)):
         msg += f'\n**#{pos + 1} {name}** — `{format(xp)} XP | {format(xp / 5)} XP/min`'
-    msg += f'\n**Total: ** `{sum([item[1] for item in xp_diff])} XP`'
+    msg += f'\n**Total: ** `{format(sum([item[1] for item in xp_diff]))} XP`'
     channel = bot.get_channel(764955778523332698)
     if channel is not None:
         await channel.send(msg)
 
 def format(n : float):
     names = ['',' Thousand',' Million',' Billion',' Trillion']
+    if (n < 1000000): return n
     pos = max(0, min(len(names) - 1, int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))))
     return f'{round(n / 10 ** (3 * pos), 2):g}{names[pos]}'
