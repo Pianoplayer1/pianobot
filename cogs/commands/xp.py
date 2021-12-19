0788d597e17cfe48fd9983323fe01ec9d0a22d8a
Pianoplayer1 <alexander@capitain.me>
2021-12-19 16:36:01 +0100

    add final option to xp.py

--- cogs/commands/xp.py
+++ cogs/commands/xp.py
@@ -13,13 +13,16 @@ class XP(commands.Cog):
                         brief = 'Outputs guild experience contribution for the current week.',
                         help = 'This command returns a list with the amount of guild experience members of Eden have gained in the current week.',
                         usage = '')
-    async def gxp(self, ctx : commands.Context):
+    async def xp(self, ctx: commands.Context, args: str = ''):
         day = datetime.combine(datetime.utcnow().date(), datetime.min.time())
         day = day - timedelta(days = day.weekday())
 
         columns = [column[0] for column in self.bot.query('SELECT "column_name" FROM "information_schema"."columns" WHERE "table_name" = \'guildXP\';')]
         oldest_data = self.bot.query('SELECT * FROM "guildXP" WHERE time = %s;', day)[0]
         newest_data = self.bot.query('SELECT * FROM "guildXP" ORDER BY time DESC LIMIT 1;')[0]
+        if args == 'final':
+            newest_data = oldest_data
+            oldest_data = self.bot.query('SELECT * FROM "guildXP" WHERE time = %s;', day - timedelta(days = 7))[0]
         start_time = round((oldest_data[0] - datetime(1970, 1, 1)).total_seconds())
         end_time = round((newest_data[0] - datetime(1970, 1, 1)).total_seconds())
         differences = {columns[i]: newest_data[i] - oldest_data[i] for i in range(1, len(columns))}
@@ -31,7 +34,7 @@ class XP(commands.Cog):
         descending_table = table(table_cols, descending_data, 5, 15, True, '(Descending Order)', f'<t:{start_time}> - <t:{end_time}>')
         await paginator(self.bot, ctx, descending_table, None, ascending_table)
     
-    def format(self, n : float):
+    def format(self, n: float):
         names = ['',' Thousand',' Million',' Billion',' Trillion']
         pos = max(0, min(len(names) - 1, int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))))
         return f'{n / 10 ** (3 * pos):.2f}{names[pos]}'
