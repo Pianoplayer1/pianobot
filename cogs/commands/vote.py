4fd8d577fa6fd6b981d55347eed2dac6113dafe8
Pianoplayer1 <alexander@capitain.me>
2021-12-28 21:45:22 +0100

    finish vote.py

--- cogs/commands/vote.py
+++ cogs/commands/vote.py
@@ -1,5 +1,11 @@
+from asyncio import gather
+from datetime import datetime, timezone
+
+from corkus.objects import Member
+
 from discord import Message
 from discord.ext import commands
+from discord.utils import get
 
 from ..bot import Pianobot
 from ..utils.permissions import check_permissions
@@ -8,18 +14,69 @@ class Vote(commands.Cog):
     def __init__(self, bot: Pianobot):
         self.bot = bot
 
-    @commands.command(name='vote',
-                      hidden=True)
-    async def vote(self, ctx: commands.Context, new: str):
-        if not check_permissions(ctx.author, ctx.channel, 'administrator'):
+    @commands.command(name='vote', hidden=True)
+    async def vote(self, ctx: commands.Context):
+        if not check_permissions(ctx.author, ctx.channel, 'manage_channels'):
             return
 
+        first_msg: Message = await ctx.send('Please wait until I have posted every vote before you start voting, I will notify you once you can vote.')
+
+        ranks = ['Owner', 'Chief', 'Strategist', 'Captain', 'Recruiter', 'Recruit']
         eden = await self.bot.corkus.guild.get('Eden')
-        for member in eden.members[:4]:
-            msg: Message = await ctx.send(f'{member.name} ({member.rank})')
-            await msg.add_reaction(':white_check_mark:')
-            await msg.add_reaction(':no_entry:')
-            await msg.add_reaction('man_shrugging')
+        results = await gather(*[self.fetch(member) for member in eden.members])
+        results = sorted(results, key=lambda x: (ranks.index(x['rank']), x['raw_time'], x['name']))
+        await gather(*[self.send(ctx, member) for member in results])
+
+        message = '''
+@everyone as decided somewhere above here, we will now vote on **every single member**'s rank, regardless of their current one! This was an idea to kind of clean up the rank distribution we have right now. In the above votes, you will find a member's ingame name, rank and inactivity time. Please take the time to look at each member and decide on their rank, even though it will take some time.
+The reactions symbolize the following:
+:baby:  - Citizen (Recruit)
+:sanglasis:  - Legionnaire (Recruiter: able to access public bank)
+:military_helmet:  - Legate (Captain: able to start wars)
+:warning:  - Tribune (Strategist: able to do economy & access private bank)
+:exclamation:  - Senate (Strategist)
+:bangbang:  - Dux (Chief: able to manage the guild ingame, e.g. when it comes to rewards, allies, hq, banner...)
+
+The exclamation marks aren't there without a reason, remember that everyone with three stars and above is able to manage the guild in some ways and to impact our relations to other guilds, especially those who get into the Senate.
+For the votes on <@235781853179543553>, <@290191000813568001>, <@718558765988970616>, <@284500595920863233>, <@232178035841957899> and <@437305878257860608>, consider their Senate applications in #elections too.
+Please refrain from memeing around on those votes, it's simply faster if you stay on topic and just vote.
+                     
+Start voting at the top by clicking here: '''
+
+        await ctx.send(message + first_msg.jump_url)
+        await first_msg.edit(content='Start voting below here (although you can of course ignore Yugito\'s vote...)')
+
+    async def fetch(self, member: Member):
+        player = await member.fetch_player()
+
+        if player.online:
+            days_offline = 0
+            display_time = 'Online'
+        else:
+            diff = datetime.now(timezone.utc) - player.last_online
+            days_offline = diff.days + (diff.seconds / 86400)
+            value = days_offline
+            unit = 'day'
+            if value < 1:
+                value *= 24
+                unit = 'hour'
+                if value < 1:
+                    value *= 60
+                    unit = 'minute'
+            if round(value) != 1:
+                unit += 's'
+            display_time = f'{round(value)} {unit}'
+        return {'name': member.username.replace('_', '\\_'), 'rank': member.rank.value.title(),
+                'last_seen': display_time, 'raw_time': days_offline}
+    
+    async def send(self, ctx: commands.Context, member: dict):
+        msg: Message = await ctx.send(f'{member["name"]} ({member["rank"]}) - {member["last_seen"]}')
+        await msg.add_reaction('👶')
+        await msg.add_reaction(get(ctx.guild.emojis, name='sanglasis'))
+        await msg.add_reaction('🪖')
+        await msg.add_reaction('⚠️')
+        await msg.add_reaction('❗')
+        await msg.add_reaction('‼️')
 
 def setup(bot: Pianobot):
     bot.add_cog(Vote(bot))
