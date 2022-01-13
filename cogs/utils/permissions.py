ebeb815b94a4b13c69fdd010a7acbd0efae32848
Pianoplayer1 <74676416+Pianoplayer1@users.noreply.github.com>
2022-01-13 08:31:16 +0100

    Update permissions.py

--- cogs/utils/permissions.py
+++ cogs/utils/permissions.py
@@ -8,7 +8,7 @@ def check_permissions(member : Union[Member, User], channel : TextChannel, *perm
         if member is None:
             return False
 
-    all_permissions = ['create_instant_invite', 'kick_members', 'ban_members', 'administrator', 'manage_channels', 'manage_guild', 'add_reactions', 'view_audit_log', 'priority_speaker', 'stream', 'view_channel', 'send_messages', 'send_tts_messages', 'manage_messages', 'embed_links', 'attach_files', 'read_message_history', 'mention_everyone', 'use_external_emojis', 'view_guild_insights', 'connect', 'speak', 'mute_members', 'deafen_members', 'move_members', 'use_vad', 'change_nickname', 'manage_nicknames', 'manage_roles', 'manage_webhooks', 'manage_emojis', 'use_slash_commands', 'request_to_speak']
+    all_permissions = ['create_instant_invite', 'kick_members', 'ban_members', 'administrator', 'manage_channels', 'manage_guild', 'add_reactions', 'view_audit_log', 'priority_speaker', 'stream', 'view_channel', 'send_messages', 'send_tts_messages', 'manage_messages', 'embed_links', 'attach_files', 'read_message_history', 'mention_everyone', 'use_external_emojis', 'view_guild_insights', 'connect', 'speak', 'mute_members', 'deafen_members', 'move_members', 'use_vad', 'change_nickname', 'manage_nicknames', 'manage_roles', 'manage_webhooks', 'manage_emojis', 'use_slash_commands', 'request_to_speak', 'manage_events', 'manage_threads', 'create_public_threads', 'create_private_threads', 'use_external_stickers', 'send_messages_in_threads', 'start_embedded_activities', 'moderate_members']
     value = channel.permissions_for(member).value
 
     member_permissions = [perm for i, perm in enumerate(all_permissions) if value & pow(2, i) > 0]
