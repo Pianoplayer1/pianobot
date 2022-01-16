473e454d2d7549be403bdf9bea0ea0f762779ebf
Pianoplayer1 <alexander@capitain.me>
2022-01-16 18:52:06 +0100

    update logging levels

--- launcher.py
+++ launcher.py
@@ -1,9 +1,10 @@
-from logging import basicConfig, WARNING
+from logging import basicConfig, getLogger, WARNING, ERROR
 from os import getenv
 
 from pianobot import Pianobot
 
 basicConfig(level = WARNING)
+getLogger("discord.gateway").setLevel(ERROR)
 
 bot = Pianobot()
 bot.run(getenv('TOKEN'))
