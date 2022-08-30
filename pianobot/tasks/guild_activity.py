aa1fee57c5e22aab721901bacc0ed43face4fae9
Pianoplayer1 <alexander@capitain.me>
2022-08-30 20:08:01 +0200

    fix guild_activity.py

--- pianobot/tasks/guild_activity.py
+++ pianobot/tasks/guild_activity.py
@@ -10,16 +10,15 @@ if TYPE_CHECKING:
 async def guild_activity(bot: Pianobot) -> None:
     guilds: dict[str, int | None] = dict()
 
-    for guild_name, online_members in await gather(*[fetch(bot, guild) for guild in guilds]):
-        guilds[guild_name] = online_members
+    for name, online_members in await gather(*[fetch(bot, guild) for guild in bot.tracked_guilds]):
+        guilds[name] = online_members
 
-    print(guilds)
     await bot.database.guild_activity.add(guilds)
 
 
-async def fetch(bot: Pianobot, name: str) -> tuple[str, int] | None:
+async def fetch(bot: Pianobot, name: str) -> tuple[str, int | None]:
     response = await bot.session.get(f'https://web-api.wynncraft.com/api/v3/guild/{name}')
     if response.status != 200:
-        return None
+        return name, None
     guild = await response.json()
     return name, guild['online_members']
