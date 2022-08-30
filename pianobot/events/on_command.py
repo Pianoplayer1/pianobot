2bf54ee23adc722d547c2c5efb6f833380e20da0
Pianoplayer1 <alexander@capitain.me>
2022-08-30 16:14:05 +0200

    add command listener

---
+++ pianobot/events/on_command.py
@@ -0,0 +1,19 @@
+from discord.ext.commands import Bot, Cog, Context
+
+from pianobot import Pianobot
+
+
+class OnCommand(Cog):
+    def __init__(self, bot: Pianobot) -> None:
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_command(self, ctx: Context[Bot]) -> None:
+        self.bot.logger.info(
+            f'{ctx.guild if ctx.guild is None else ctx.guild.name} -'
+            f' {ctx.author.name}#{ctx.author.discriminator}: {ctx.message.content}'
+        )
+
+
+async def setup(bot: Pianobot) -> None:
+    await bot.add_cog(OnCommand(bot))
