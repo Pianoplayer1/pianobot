bb63056a593fd854452c3bd7045b06ab0dc5db91
Pianoplayer1 <alexander@capitain.me>
2022-09-01 09:48:40 +0200

    adjust logging level

--- launcher.py
+++ launcher.py
@@ -6,6 +6,7 @@ from pianobot import Pianobot
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
+    logging.getLogger('discord.gateway').setLevel(logging.INFO)
 
     bot = Pianobot()
     bot.run(os.getenv('TOKEN') or '', log_handler=None)
