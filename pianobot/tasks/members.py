da788290fb66755b37cd7482c3c2485628cfb8ec
Pianoplayer1 <alexander@capitain.me>
2022-10-13 16:26:08 +0200

    fix members.py

--- pianobot/tasks/members.py
+++ pianobot/tasks/members.py
@@ -24,30 +24,25 @@ async def members(bot: Pianobot) -> None:
     }
 
     for corkus_member in guild_members:
-        if corkus_member.uuid.hex in saved_members:
-            saved_member = saved_members.pop(corkus_member.uuid.hex, None)
-            if saved_member is None:
+        saved_member = saved_members.pop(corkus_member.uuid.hex, None)
+        if saved_member is None:
+            LOGGER.info(
+                f'{corkus_member.username} has joined Eden as a {corkus_member.rank.name}!'
+            )
+            await bot.database.members.add(
+                corkus_member.uuid, corkus_member.username, corkus_member.rank.name
+            )
+        else:
+            if corkus_member.username != saved_member[0]:
                 LOGGER.info(
-                    f'{corkus_member.username} has joined Eden as a {corkus_member.rank.name}!'
+                    f'{saved_member[0]} has changed their name to {corkus_member.username}!'
                 )
-                await bot.database.members.add(
-                    corkus_member.uuid, corkus_member.username, corkus_member.rank.name
+                await bot.database.members.update_name(corkus_member.uuid, corkus_member.username)
+            if corkus_member.rank.name != saved_member[1]:
+                LOGGER.info(
+                    f'{corkus_member.username}: {saved_member[1]} -> {corkus_member.rank.name}'
                 )
-            else:
-                if corkus_member.username != saved_member[0]:
-                    LOGGER.info(
-                        f'{saved_member[0]} has changed their name to {corkus_member.username}!'
-                    )
-                    await bot.database.members.update_name(
-                        corkus_member.uuid, corkus_member.username
-                    )
-                if corkus_member.rank.name != saved_member[1]:
-                    LOGGER.info(
-                        f'{corkus_member.username}: {saved_member[1]} -> {corkus_member.rank.name}'
-                    )
-                    await bot.database.members.update_rank(
-                        corkus_member.uuid, corkus_member.rank.name
-                    )
+                await bot.database.members.update_rank(corkus_member.uuid, corkus_member.rank.name)
 
     for uuid, left_member in saved_members.items():
         LOGGER.info(f'{left_member[0]} ({left_member[1]}) has left Eden!')
