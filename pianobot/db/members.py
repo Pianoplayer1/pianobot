92a939c4c2df504741302c89ce905b35c9511ae7
Pianoplayer1 <alexander@capitain.me>
2022-10-14 17:19:15 +0200

    change member tracking to send updates to a text channel

--- pianobot/db/members.py
+++ pianobot/db/members.py
@@ -1,17 +1,26 @@
+from datetime import datetime
 from uuid import UUID
 
 from pianobot.db import Connection
 
 
 class Member:
-    def __init__(self, uuid: UUID, name: str, rank: str) -> None:
+    def __init__(
+        self, uuid: UUID, join_date: datetime, name: str, rank: str, contributed_xp: int
+    ) -> None:
         self._uuid = uuid
+        self._join_date = join_date
         self._name = name
         self._rank = rank
+        self._contributed_xp = contributed_xp
 
     @property
     def uuid(self) -> UUID:
         return self._uuid
+
+    @property
+    def join_date(self) -> datetime:
+        return self._join_date
 
     @property
     def name(self) -> str:
@@ -21,18 +30,32 @@ class Member:
     def rank(self) -> str:
         return self._rank
 
+    @property
+    def contributed_xp(self) -> int:
+        return self._contributed_xp
+
 
 class MemberTable:
     def __init__(self, con: Connection) -> None:
         self._con = con
 
     async def get_all(self) -> list[Member]:
-        result = await self._con.query('SELECT uuid, name, rank FROM members')
-        return [Member(row[0], row[1], row[2]) for row in result]
+        result = await self._con.query(
+            'SELECT uuid, join_date, name, rank, contributed_xp FROM members'
+        )
+        return [Member(row[0], row[1], row[2], row[3], row[4]) for row in result]
 
-    async def add(self, uuid: UUID, name: str, rank: str) -> None:
+    async def add(
+        self, uuid: UUID, joined_at: datetime, name: str, rank: str, contributed_xp: int
+    ) -> None:
         await self._con.execute(
-            f'INSERT INTO members (uuid, name, rank) VALUES ($1, $2, $3)', uuid, name, rank
+            f'INSERT INTO members (uuid, name, join_date, rank, contributed_xp)'
+            f' VALUES ($1, $2, $3, $4, $5)',
+            uuid,
+            joined_at,
+            name,
+            rank,
+            contributed_xp,
         )
 
     async def remove(self, uuid: UUID) -> None:
@@ -43,3 +66,8 @@ class MemberTable:
 
     async def update_rank(self, uuid: UUID, rank: str) -> None:
         await self._con.execute('UPDATE members SET rank = $1 WHERE uuid = $2', rank, uuid)
+
+    async def update_contributed_xp(self, uuid: UUID, contributed_xp: int) -> None:
+        await self._con.execute(
+            'UPDATE members SET contributed_xp = $1 WHERE uuid = $2', contributed_xp, uuid
+        )
--- pianobot/tasks/members.py
+++ pianobot/tasks/members.py
@@ -4,48 +4,124 @@ from logging import getLogger
 from typing import TYPE_CHECKING
 
 from corkus.errors import CorkusTimeoutError
+from discord import Embed
+from discord.utils import format_dt
+
+from pianobot.db.members import Member
 
 if TYPE_CHECKING:
     from pianobot import Pianobot
 
-LOGGER = getLogger('tasks.members')
+RANKS = ['Recruit', 'Recruiter', 'Captain', 'Strategist', 'Chief', 'Owner']
 
 
 async def members(bot: Pianobot) -> None:
     try:
         guild_members = (await bot.corkus.guild.get('Eden')).members
     except CorkusTimeoutError:
-        LOGGER.warning('Error when fetching guild data of `Eden`')
+        getLogger('tasks.members').warning('Error when fetching guild data of `Eden`')
         return
 
     database_members = await bot.database.members.get_all()
-    saved_members: dict[str, tuple[str, str]] = {
-        member.uuid.hex: (member.name, member.rank) for member in database_members
-    }
+    saved_members: dict[str, Member] = {member.uuid.hex: member for member in database_members}
 
     for corkus_member in guild_members:
         saved_member = saved_members.pop(corkus_member.uuid.hex, None)
         if saved_member is None:
-            LOGGER.info(
-                f'{corkus_member.username} has joined Eden as a {corkus_member.rank.name}!'
-            )
             await bot.database.members.add(
-                corkus_member.uuid, corkus_member.username, corkus_member.rank.name
+                corkus_member.uuid,
+                corkus_member.join_date,
+                corkus_member.username,
+                corkus_member.rank.name.capitalize(),
+                corkus_member.contributed_xp,
+            )
+            embed_content = (
+                f'{corkus_member.username} has joined Eden'
+                f' {format_dt(corkus_member.join_date, "R")}\n\n'
+            )
+            try:
+                player = await corkus_member.fetch_player()
+                embed_content += (
+                    f'First join: {format_dt(player.join_date)}\n'
+                    f'Playtime: {round(player.playtime * 4.7 / 60)} hours\n'
+                    f'Total level: {player.combined_level}'
+                )
+            except CorkusTimeoutError:
+                getLogger('tasks.members').warning('Error when fetching player data')
+
+            await send_embed(
+                bot,
+                title=f'Guild Join: {corkus_member.username}',
+                content=embed_content,
+                color=0x00FF00,
+                uuid=corkus_member.uuid.hex,
             )
         else:
-            if corkus_member.username != saved_member[0]:
-                LOGGER.info(
-                    f'{saved_member[0]} has changed their name to {corkus_member.username}!'
-                )
+            if corkus_member.username != saved_member.name:
                 await bot.database.members.update_name(corkus_member.uuid, corkus_member.username)
-            if corkus_member.rank.name != saved_member[1]:
-                LOGGER.info(
-                    f'{corkus_member.username}: {saved_member[1]} -> {corkus_member.rank.name}'
+                embed_content = (
+                    f'{saved_member.name} has changed their name to {corkus_member.username}!\n\n'
+                    f'Guild rank: {corkus_member.rank.name.capitalize()}\n'
+                    f'Old name: {saved_member.name}\n'
+                    f'New name: {corkus_member.username}'
                 )
-                await bot.database.members.update_rank(corkus_member.uuid, corkus_member.rank.name)
+                await send_embed(
+                    bot,
+                    title=f'Name Change: {corkus_member.username}',
+                    content=embed_content,
+                    color=0x88FFFF,
+                    uuid=corkus_member.uuid.hex,
+                )
+            if corkus_member.rank.name != saved_member.rank:
+                new_rank = corkus_member.rank.name.capitalize()
+                await bot.database.members.update_rank(corkus_member.uuid, new_rank)
+                is_promotion = RANKS.index(new_rank) > RANKS.index(saved_member.rank)
+                embed_content = (
+                    f'{corkus_member.username} has been'
+                    f' {"promoted" if is_promotion else "demoted"}!\n\n'
+                    f'Old rank: {saved_member.rank}\n'
+                    f'New rank: {new_rank}'
+                )
+                await send_embed(
+                    bot,
+                    title=(
+                        f'Guild {"promotion" if is_promotion else "demotion"}:'
+                        f' {corkus_member.username}'
+                    ),
+                    content=embed_content,
+                    color=0x88FF88 if is_promotion else 0xFF8888,
+                    uuid=corkus_member.uuid.hex,
+                )
+            if corkus_member.contributed_xp != saved_member.contributed_xp:
+                await bot.database.members.update_contributed_xp(
+                    corkus_member.uuid, corkus_member.contributed_xp
+                )
 
-    for uuid, left_member in saved_members.items():
-        LOGGER.info(f'{left_member[0]} ({left_member[1]}) has left Eden!')
+    for uuid, member in saved_members.items():
         await bot.database.members.remove(
             next(m for m in database_members if m.uuid.hex == uuid).uuid
         )
+        embed_content = (
+            f'{member.name} has left Eden!\n\n'
+            f'Joined at: {format_dt(member.join_date)}\n'
+            f'Last rank: {member.rank}\n'
+            f'XP contributed: {member.contributed_xp}'
+        )
+        await send_embed(
+            bot,
+            title=f'Guild Leave: {member.name}',
+            content=embed_content,
+            color=0xFF0000,
+            uuid=uuid,
+        )
+
+
+async def send_embed(bot: Pianobot, *, title: str, content: str, color: int, uuid: str) -> None:
+    embed = Embed(
+        title=title,
+        description=content,
+        color=color,
+    )
+    embed.set_thumbnail(url=f'https://mc-heads.net/avatar/{uuid}')
+    if bot.member_update_channel is not None:
+        await bot.member_update_channel.send(embed=embed)
